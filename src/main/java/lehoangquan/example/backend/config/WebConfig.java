6b29d56e5ea9ec9f9f115401a9399b23b0b22e08
Le Hoang Quan <lehoangquan0305@example.com>
2025-12-13 00:42:01 +0700

    final

---
+++ src/main/java/lehoangquan/example/backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package lehoangquan.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://restaurant-client-chi.vercel.app", "https://restaurant-admin-alpha.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
